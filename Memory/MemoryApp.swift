7309ab0f06f79ff98d9c9f17ee3de18034cf6c2f
Salman Ahmed Abdiaziz <salman.abdiaziz2001@gmail.com>
2024-07-05 13:23:35 +0100

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Salman Abdiaziz on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
